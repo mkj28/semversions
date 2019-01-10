762e6b477785bbb2693ddc7334432668b2c6fc14
Michal Matyjek <michal@gladly.com>
2019-01-10 14:29:57 -0800

    Work for reruns

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,18 @@ fi
 echo ${SSH_KEY_BASE64} | base64 -d > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
 export TZ=UTC
-COMMIT_TAG=v$(date "+%Y.%m%d").$(printf %04d $(git rev-list HEAD --count --date=local --after "yesterday"))
+COMMIT_VERSION=v$(date "+%Y.%m%d").$(printf %04d $(git rev-list HEAD --count --date=local --after "yesterday"))
 
-# tag git
-git tag $COMMIT_TAG
-git push --tags
+# check if tag already exists
+if [[ "$(git tag -l --points-at HEAD | grep ${COMMIT_VERSION} | wc -l)" -gt 0 ]]; then
+    echo "WARN: ${COMMIT_VERSION} already pointing to this commit"
+    echo COMMIT_VERSION=${COMMIT_VERSION} >> ${CF_VOLUME_PATH}/env_vars_to_export
+    exit 0
+fi
+
+# tag git and push the tag to remote
+git tag $COMMIT_VERSION
+git push origin $COMMIT_VERSION
 
 # make available for other Codefresh steps
-echo COMMIT_TAG=${COMMIT_TAG} >> ${CF_VOLUME_PATH}/env_vars_to_export
+echo COMMIT_VERSION=${COMMIT_VERSION} >> ${CF_VOLUME_PATH}/env_vars_to_export
