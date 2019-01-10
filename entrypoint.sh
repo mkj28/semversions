4a9840e0c33ee2bc195e795f17c7f061bdd1d225
Michal Matyjek <michal@gladly.com>
2019-01-09 17:22:53 -0800

    Test4 (#6)
    * Cleanup
    
    * Cleanup more

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "${CF_BRANCH_TAG_NORMALIZED}"
-
-if [[ -z "${CF_BUILD_ID}" ]]; then
-    echo "ERROR: only runs on Codefresh"
-    exit 1
-fi
-
 if [[ "${CF_BRANCH_TAG_NORMALIZED}" != "master" ]]; then
-    echo "ERROR: only working on master branch"
+    echo "ERROR: only working on master branch in Codefresh"
     exit 1
 fi
 echo ${SSH_KEY_BASE64} | base64 -d > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
