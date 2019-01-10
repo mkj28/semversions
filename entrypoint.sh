d05fef398f05b1f3a2052de39696b4780934ffed
Michal Matyjek <michal@gladly.com>
2019-01-09 17:07:57 -0800

    Add more safety

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+if [[ -z "${CF_BUILD_ID}" ]]; then
+    echo "ERROR: only runs on Codefresh"
+    exit 1
+fi
+
+if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+    echo "ERROR: only working on master branch"
+    exit 1
+fi
 echo ${SSH_KEY_BASE64} | base64 -d > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 TZ=UTC git tag v$(date "+%Y.%m%d").$(printf %04d $(git rev-list HEAD --count --date=local --after "yesterday"))
 git push --tags
