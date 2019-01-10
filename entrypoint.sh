773f188ae30d646fc628bf2556e2fadfd428990d
Michal Matyjek <michal@gladly.com>
2019-01-09 21:35:10 -0800

    Add cf_export

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,16 @@ if [[ "${CF_BRANCH_TAG_NORMALIZED}" != "master" ]]; then
     echo "ERROR: only working on master branch in Codefresh"
     exit 1
 fi
+
+# ssh key for git push
 echo ${SSH_KEY_BASE64} | base64 -d > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-TZ=UTC git tag v$(date "+%Y.%m%d").$(printf %04d $(git rev-list HEAD --count --date=local --after "yesterday"))
+
+export TZ=UTC
+COMMIT_TAG=v$(date "+%Y.%m%d").$(printf %04d $(git rev-list HEAD --count --date=local --after "yesterday"))
+
+# tag git
+git tag $COMMIT_TAG
 git push --tags
+
+# make available for other Codefresh steps
+cf_export COMMIT_TAG=$COMMIT_TAG
