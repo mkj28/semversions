dd9ec2a8a3636e2a6f5b552a19f3e3b5ca951f93
Michal Matyjek <michal@gladly.com>
2019-01-17 10:30:47 -0800

    Branch tag (#8)
    * Set tags on branch, too
    
    * Updated readme

--- semversions.sh
+++ semversions.sh
@@ -2,32 +2,35 @@
 set -eu
 
 PREFIX=v
+SUFFIX="" # for dev branches
+
+if [[ -z "${CF_BRANCH_TAG_NORMALIZED}" ]]; then
+    echo "CF_BRANCH_TAG_NORMALIZED not set"
+    exit 1
+fi
 
 if [[ "${CF_BRANCH_TAG_NORMALIZED}" != "master" ]]; then
-    echo "ERROR: only working on master branch in Codefresh"
-    exit 1
+    SUFFIX="-${CF_BRANCH_TAG_NORMALIZED}"
 fi
 
 # ssh key for git push
 echo ${SSH_KEY_BASE64} | base64 -d > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
 export TZ=UTC
-COMMIT_TIMESTAMP=$(git log -1 --format="%at")
+COMMIT_TIMESTAMP=$(git log -1 --format="%ct")
 DATE_STRING=$(date -d @${COMMIT_TIMESTAMP} "+%Y.%m%d")
 COMMITS_THIS_DAY=$(printf %04d $(git rev-list HEAD --count --date=local --after="$(date -d @${COMMIT_TIMESTAMP} "+%Y-%m-%d 00:00")"))
 
-COMMIT_VERSION=${PREFIX}${DATE_STRING}.${COMMITS_THIS_DAY}
+COMMIT_VERSION=${PREFIX}${DATE_STRING}.${COMMITS_THIS_DAY}${SUFFIX}
 
 # check if tag already exists
-if [[ "$(git tag -l --points-at HEAD | grep ${COMMIT_VERSION} | wc -l)" -gt 0 ]]; then
+if [[ "$(git tag -l --points-at HEAD | grep ${COMMIT_VERSION} | wc -l)" -eq 0 ]]; then
+    # tag git and push the tag to remote, move the tag if already existing
+    git tag --force $COMMIT_VERSION
+    git push --force --tags origin $COMMIT_VERSION
+else
     echo "INFO: ${COMMIT_VERSION} already pointing to this commit"
-    echo COMMIT_VERSION=${COMMIT_VERSION} >> ${CF_VOLUME_PATH}/env_vars_to_export
-    exit 0
 fi
-
-# tag git and push the tag to remote
-git tag $COMMIT_VERSION
-git push origin $COMMIT_VERSION
 
 # make available for other Codefresh steps
 echo COMMIT_VERSION=${COMMIT_VERSION} >> ${CF_VOLUME_PATH}/env_vars_to_export
